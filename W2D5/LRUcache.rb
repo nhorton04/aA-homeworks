0c282dd47ec5bba74f9b11ea79979d98ee5f0203
Nick <nhorton04@gmail.com>
2018-08-31 02:43:07 -0700

    add LRU Cache

---
+++ W2D5/LRUcache.rb
@@ -0,0 +1,31 @@
+class LRUCache
+  def initialize(size)
+    @size = size
+    @cache = []
+  end
+
+  def count
+    @cache.count
+  end
+
+  def add(el)
+    if @cache.include?(el)
+      @cache.delete(el)
+      @cache << el
+    elsif count >= @size
+      @cache.shift
+      @cache << el
+    else
+      @cache << el
+    end
+  end
+
+  def show
+    print @cache
+
+  end
+
+  private
+  # helper methods go here!
+
+end
