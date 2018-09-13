6232ef222cd0f920bb293904e1bcc33ef40b05f2
Nick <nhorton04@gmail.com>
2018-09-12 23:36:08 -0700

    finish, issues with mailer

---
+++ W4D4/99_cats_auth/app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+  default from: 'email@gmail.com'
+
+  def welcome_email(user)
+    @user = user
+    @url = 'http://example.com/login'
+    mail(to: user.email, subject: 'Hello user ;)')
+  end
+
+  
+end
--- W4D4/99_cats_auth/config/environments/development.rb
+++ W4D4/99_cats_auth/config/environments/development.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -27,9 +29,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
